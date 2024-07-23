1b62ebd26953b66815a74a7edef2b1aca466646c
Ruobing Wang <1298676106@qq.com>
2024-07-23 14:21:14 +0800

    加上antdv的配置

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -14,7 +15,12 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+          ElementPlusResolver(),
+          AntDesignVueResolver({
+            importStyle: false,
+          }),
+      ],
     }),
   ],
   resolve: {
